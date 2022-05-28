317ec343af1fd2ab2fad3bb4c2cd37090fbda220
iggy <allyson.nostrandd@gmail.com>
2022-05-28 14:04:58 -0700

    sucessfully made first fetch request

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
 var APIkey = "37f8c9a38ed79ac3bb509ee15e76b898"
+city= "Monroe"
+
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
+
+function getApi() {
+   fetch(queryURL)
+   .then (function (response){
+       return response.json()
+   }).then (function (data){
+       console.log(data)
+   }); 
+}
+
+getApi();
