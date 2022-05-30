970eb84ec726285395adc2bd1b1b490541d1d226
iggy <allyson.nostrandd@gmail.com>
2022-05-29 17:52:20 -0700

    temp,wind,and humidity gets appended to page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
-var APIkey = "37f8c9a38ed79ac3bb509ee15e76b898"
+var APIkey = "37f8c9a38ed79ac3bb509ee15e76b898";
+var list = document.querySelector("ul");
 // let city= "Monroe"
 
 
@@ -35,7 +36,18 @@ document.querySelector("#searchForm").addEventListener("submit", e => {
         return response.json()
     }).then (function (data){
         console.log(data)
-    });
+        let todayTemp = document.createElement("p")
+        todayTemp.textContent = `Temp: ${data.main.temp} F`
+        list.appendChild(todayTemp)
+
+        let todayWind = document.createElement("p")
+        todayWind.textContent = `Wind Speed: ${data.wind.speed}`
+        list.appendChild(todayWind)
+
+        let todayHumidity = document.createElement("p")
+        todayHumidity.textContent = `Humidity: ${data.main.humidity}` 
+        list.appendChild(todayHumidity)
+    })
 })
 
 document.querySelector("#searchForm").addEventListener("submit", e => {
